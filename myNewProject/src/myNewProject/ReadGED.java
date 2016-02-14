945bb6b9f4351e92eebc7cbbbc97c4a0cb045ea5
dszymczu <dszymczu@stevens.edu>
2016-02-14 18:16:22 -0500

    Refactor and use Local File

--- myNewProject/src/myNewProject/ReadGED.java
+++ myNewProject/src/myNewProject/ReadGED.java
@@ -1,57 +1,52 @@
 package myNewProject;
 
+import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 import java.util.Scanner;
 
 public class ReadGED {
 
-    /**
-     * @param args the command line arguments
-     * @throws java.io.FileNotFoundException
-     */
-    public static void main(String[] args) /*throws FileNotFoundException*/ {
+    public static void main(String[] args) {
     	
     	try {
-    	URL url = new URL("https://raw.githubusercontent.com/vijaytewari/myStevens/master/P03_GEDCOMFile.ged");
-        Scanner fileReader = new Scanner(url.openStream());
+	    	File gedFile = new File("~/../../gedFile.ged");
+	        Scanner fileReader = new Scanner(gedFile);
+	
+	        String[] tags = {"INDI", "NAME","SEX","BIRT","DEAT","FAMC","FAMS","FAM","MARR","HUSB","WIFE","CHIL","DIV","DATE","HEAD","TRLR","NOTE"};
+	        String level;
+	        String tag;
+	        Boolean validTag;
+	        String value;
 
-        String[] tags = {"INDI", "NAME","SEX","BIRT","DEAT","FAMC","FAMS","FAM","MARR","HUSB","WIFE","CHIL","DIV","DATE","HEAD","TRLR","NOTE"};
-        String level;
-        String tag;
-        Boolean validTag;
-        //String line;
-        String value;
-        //String nextValue;
-        //StringBuilder builder = new StringBuilder();
-
-        while(fileReader.hasNext()){
-            //Read in level
-            level = fileReader.next();
-
-            //Read tag
-            tag = fileReader.next();
-
-            //Read in the rest of the line
-            value = fileReader.nextLine();
-
-            //Print out the information
-            System.out.println(level+" "+tag+" "+value);
-            System.out.println("Level: " + level);
-
-            //Check for invalid tag
-            validTag = false;
-            for(int i=0;i<tags.length;i++){
-                if(tag.equals(tags[i])){
-                    validTag = true;
-                }
-            }
-            if(!validTag){
-                tag = "Invalid tag";
-            }
-            System.out.println("Tag: " + tag);
-        }
-        
+	        while(fileReader.hasNext()){
+	            //Read in level
+	            level = fileReader.next();
+	
+	            //Read tag
+	            tag = fileReader.next();
+	
+	            //Read in the rest of the line
+	            value = fileReader.nextLine();
+	
+	            //Print out the information
+	            System.out.println(level+" "+tag+" "+value);
+	            System.out.println("Level: " + level);
+	
+	            //Check for invalid tag
+	            validTag = false;
+	            for(int i = 0; i < tags.length; i++){
+	                if(tag.equals(tags[i])){
+	                    validTag = true;
+	                }
+	            }
+	            if(!validTag){
+	                tag = "Invalid tag";
+	            }
+	            System.out.println("Tag: " + tag);
+	        }
+	        
+	        // Done reading
+	        fileReader.close();
     	}
     	catch(IOException ex) {
     		   // there was some connection problem, or the file did not exist on the server,
